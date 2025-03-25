a4ada0ccba8e8d23321bab1f8f6f3619989a3ff8
Denos Kume <110854822+denoskume@users.noreply.github.com>
2025-03-25 13:02:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to handle template selection
+function selectTemplate(template) {
+    alert(`You selected ${template}!`);
+    // Logic for loading the template into the editor can be added here
+}
